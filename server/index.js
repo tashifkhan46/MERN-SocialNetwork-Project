0586c05802ac77f6314fee9f99dbe62d028c0a11
tash <tashifkhan46@gmail.com>
2022-12-24 18:19:01 +0530

    First commit-entire project loaded

---
+++ server/index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import router from './routes/auth.js';
+import './models/user.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(router);
+
+
+const customMiddleware = (req,res,next) =>{
+    console.log("Middleware Executed!!!");
+    next();
+}
+//app.use(customMiddleware);
+app.get("/home",(req,res)=>{
+    console.log('Hello from Home Page'); //terminal
+    res.send("I am Home page")  //Browser
+})
+
+app.get("/login",customMiddleware,(req,res)=>{
+    console.log('Hello from Login Page'); //terminal
+    res.send("I am Login Page")  //Browser
+})
+
+app.listen(PORT,()=>{
+    console.log("SERVER RUNNING ON:",PORT)// Terminal
+})
+
+
+
+
+
+
+
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://socialnetwork:socialnetwork@cluster0.qnbip.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL).then(()=>{
+    console.log(`SERVER CONNECT DATABASE THROUGHT: ${PORT}`)
+})
+
